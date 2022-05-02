b25eb2e1af05f6dcdc1b0a7ec320be3fc21f6de3
TenzinYangzom20 <tenzin.yangzom@mca.christuniversity.in>
2022-05-02 16:05:30 +0530

    application

--- Controller/Application.js
+++ Controller/Application.js
@@ -1,5 +1,9 @@
 const async = require("async");
 const res = require("express/lib/response");
+
+const xl = require("excel4node");
+const { sendExcelFile } = require("../Services/ExcelService");
+
 exports.getApplicationMstData = (req, res) => {
   var query =
     "SELECT * from application_mst WHERE student_id = " +
@@ -16,33 +20,208 @@ exports.getApplicationMstData = (req, res) => {
         (apMst, key, loopCompleted) => {
           // for each item in loop do this
           var query =
-        "SELECT * FROM application WHERE application_id = " + apMst.id;
-      database.query(query,async (err, applicationFromDB) => {
-        apMst.application = applicationFromDB[0];
-        data.push(apMst);
-        console.log("looping");
-        apMst.job =  await getJobMstDataByJobMstId(apMst.job_id)
-        loopCompleted()
-      });
+            "SELECT * FROM application WHERE application_id = " + apMst.id;
+          database.query(query, async (err, applicationFromDB) => {
+            apMst.application = applicationFromDB[0];
+            data.push(apMst);
+            //console.log("looping");
+            apMst.job = await getJobMstDataByJobMstId(apMst.job_id);
+            loopCompleted();
+          });
         },
         (err) => {
           // after loop is complete do this
-          if(err) return 
+          if (err) return;
           res.json({ success: 1, data: { application_mst } });
-          console.log("Data Sent");
+          //console.log("Data Sent");
         }
-      );      
+      );
     }
   });
 };
 
 function getJobMstDataByJobMstId(id) {
-  return new Promise((data, error)=>{
+  return new Promise((data, error) => {
     var query = "SELECT * FROM job_mst WHERE id =" + id;
 
-  database.query(query, (err, job_mst)=>{
-    if (err) error(err);
-    job_mst.length<1? data(null) : data(job_mst[0] )    
-  })
-  })
+    database.query(query, (err, job_mst) => {
+      if (err) error(err);
+      job_mst.length < 1 ? data(null) : data(job_mst[0]);
+    });
+  });
 }
+
+function getStudentDetails(id) {
+  return new Promise((data, error) => {
+    var query = "SELECT * FROM student WHERE  id =" + id;
+    database.query(query, (err, student) => {
+      if (err) error(err);
+      var query = "SELECT * FROM login WHERE id = " + student[0].login_id;
+      database.query(query, async (err, dataFromDB) => {
+        student[0].login = dataFromDB[0];
+        data(student[0]);
+      });
+    });
+  });
+}
+
+// exports.saveNewApplication=(req, res)=>{
+//   // get data from client and put into application table
+//   INSERT INTO `recruitz`.`application_mst` (`student_id`, `job_id`) VALUES ('2', '2');
+//   INSERT INTO `recruitz`.`application` (`application_id`, `status`) VALUES ('2', 'w');
+
+// }
+
+exports.getAllUserApplications = (req, res) => {
+  const wb = new xl.Workbook();
+
+  // all application mst order by date created desc
+  database.query("SELECT * FROM application_mst", (err, application_msts) => {
+    // var data = [];
+    async.forEach(
+      application_msts,
+      (application_mst, done) => {
+        // for each item in loop do this
+        var query =
+          "SELECT * FROM application WHERE application_id = " +
+          application_mst.id;
+        database.query(query, async (err, applicationFromDB) => {
+          application_mst.application = applicationFromDB[0];
+          // data.push(application_mst);
+          //console.log("looping");
+          application_mst.job = await getJobMstDataByJobMstId(
+            application_mst.job_id
+          );
+          application_mst.student = await getStudentDetails(
+            application_mst.student_id
+          );
+
+          done();
+        });
+      },
+      (err) => {
+        // after loop is complete do this
+        if (err) return;
+        res.json({ success: 1, data: application_msts });
+        //console.log("Data Sent");
+      }
+    );
+  });
+};
+
+exports.getApplicationsExcel = (req, res) => {
+  const wb = new xl.Workbook();
+
+  // all application mst order by date created desc
+  database.query("SELECT * FROM application_mst", (err, application_msts) => {
+    // var data = [];
+    var data = [];
+    var cols = [
+      "Login Id",
+      "Login Email",
+      "Name",
+      "Role",
+      "Company",
+      "Type",
+      "Location",
+      "Status",
+    ];
+    async.forEach(
+      application_msts,
+      (application_mst, done) => {
+        // for each item in loop do this
+        var query =
+          "SELECT * FROM application WHERE application_id = " +
+          application_mst.id;
+        database.query(query, async (err, applicationFromDB) => {
+          application_mst.application = applicationFromDB[0];
+
+          //console.log("looping");
+          application_mst.job = await getJobMstDataByJobMstId(
+            application_mst.job_id
+          );
+          application_mst.student = await getStudentDetails(
+            application_mst.student_id
+          );
+          data.push([application_mst.id, 
+            application_mst.student.login.email,
+            application_mst.student.login.name,
+            application_mst.job.role,
+            application_mst.job.company,
+            application_mst.job.type,
+            application_mst.job.location,
+            application_mst.application.status
+
+          ]);
+          done();
+        });
+      },
+      (err) => {
+        // after loop is complete do this
+        if (err) return;
+        if (req.query.download == "true")
+          sendExcelFile(
+            data,
+            cols,
+            res,
+            "StudentApplication@" + new Date().toLocaleString()
+          );
+        else res.json({ data: data, success: 1 });
+        //console.log("Data Sent");
+      }
+    );
+  });
+};
+
+// exports.getApplicationsExcel = (req, res) => {
+//   const wb = new xl.Workbook();
+//   database.query("SELECT * from login", (err, logins) => {
+//     if (err) res.status(400).json({ success: 0, error: err });
+//     var data = [];
+//     var cols = [
+//       "Login Id",
+//       "Login Email",
+//       "Name",
+//       "Role",
+//       "Company",
+//       "Type",
+//       "Location",
+//       "Status",
+//     ];
+//     async.forEach(
+//       logins,
+//       (login, done) => {
+//         database.query(
+//           "SELECT * from student WHERE login_id=" + login.id,
+//           (err, student) => {
+//             // console.log(student);
+//             student = student[0];
+//             data.push([
+//               login.id,
+//               login.email,
+//               login.name,
+//               student.bio,
+//               student.about,
+//               student.github,
+//               student.linkedin,
+//               student.phone,
+//               student.skills,
+//             ]);
+//             done();
+//           }
+//         );
+//       },
+//       (err) => {
+//         if (err) console.log(err);
+//         if (req.query.download == "true")
+//           sendExcelFile(
+//             data,
+//             cols,
+//             res,
+//             "StudentApplication@" + new Date().toLocaleString()
+//           );
+//         else res.json({ data: data, success: 1 });
+//       }
+//     );
+//   });
+// };
--- Routes/Application.js
+++ Routes/Application.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const {  getApplicationMstData } = require("../Controller/Application");
 const { getUserById } = require("../Controller/User");
+const { getAllUserApplications } = require("../Controller/Application");
+const { sendExcelFile } = require("../Services/ExcelService");
+
+const {getApplicationsExcel} = require("../Controller/Application");
 
 const router = express.Router()
 
 
 router.param("userId", getUserById)
-router.get("/application/:userId",(req,res,next)=>{
-    console.log('eeee');
-    req.testdata = 'new'
-    next()
-},getApplicationMstData)
+router.get("/application/:userId",getApplicationMstData)
+
+router.get("/apps/:userId", (req, res)=>{
+    if(req.query.download=='true'){
+        console.log(11);
+        getApplicationsExcel(req, res)
+    }else{
+        console.log(22);
+        getAllUserApplications(req, res)
+    }
+});
+
+
 
 module.exports=router
+
+
